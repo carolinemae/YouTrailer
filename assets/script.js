724dd40421ddf082d67a29af7df8ad936a143554
Ben Smerd <bsmerd@adam.com.au>
2022-07-21 19:53:58 +0930

    update.

--- assets/script.js
+++ assets/script.js
@@ -4,54 +4,22 @@ var movieResult;
 var movieInfo = $('#movie-info')
 
 
-function searchMovie(title) {
-
-var sliderRating = document.getElementById("myRangeRating");
-var outputRating = document.getElementById("rating");
-outputRating.innerHTML = sliderRating.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-sliderRating.oninput = function() {
-var ratingLow = outputRating.innerHTML = this.value;
-}
-
-var sliderYear = document.getElementById("myRangeYear");
-var outputYear = document.getElementById("year");
-outputYear.innerHTML = sliderYear.value; // Display the default slider value
-
-
-// Update the current slider value (each time you drag the slider handle)
-sliderYear.oninput = function() {
-var yearLow = outputYear.innerHTML = this.value;
-}
-
-var genres = $('#genre :selected').text()
-
-console.log(genres);
-
-for (var i = 0; i < localStorage.length; i++) {
-    var title = localStorage.getItem(i);
-}
-var keyCount = 0;
-}
-
-// https://imdb-api.com/API/AdvancedSearch/k_yu9dk035?title=inception&user_rating=1.0,10&release_date=1950-01-01,2022-01-01&genres=action&sort=moviemeter,desc
-function searchMovie(title, year, rating) {
+function searchMovie(titleOne) {
     var rating = $("#rating").text()
     var year = $("#year").text()
     var genres = $('#genre :selected').text()
-
+    console.log("Second work")
     //APi Link goes here
     var urlMovie = "https://imdb-api.com/API/AdvancedSearch/k_q5dx85dn?title=" + title + "&user_rating=" + rating + ",10&release_date=" + year + "-01-01,2022-01-01&genres=" + genres;
-
+    console.log("third work")
     if (title == "") {
         alert('Please insert a movie title.')
-        console.log(title);
     } else {
         $.ajax({
             url: urlMovie,
             method: "GET"
         }).then(function (response) {
+            console.log("response OK")
             var response = response.results
             movieResult = $("#movies").append("<div>");
             movieResult.empty();
@@ -162,7 +130,7 @@ searchButton.click(() => {
     var local = localStorage.setItem("keyCount", searchInput);
     var history = $(".history")
     history.append("<li>" +  searchInput + "</li>");           
-
+    console.log("Worl")
     searchMovie(searchInput);
 
 });
